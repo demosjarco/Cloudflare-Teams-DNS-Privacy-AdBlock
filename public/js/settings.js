e3219abc8611dcba321c485641420db547d0249f
Victor <1209494+demosjarco@users.noreply.github.com>
2021-12-26 19:02:27 -0700

    Sample settings form

---
+++ public/js/settings.js
@@ -0,0 +1,8 @@
+// $("div#settingsModal").click(function () {
+// 	e.preventDefault();
+// });
+$(function () {
+	$("div#settingsModal form").submit((event) => {
+		console.log(event);
+	});
+});
--- public/js/setup.js
+++
@@ -1,6 +0,0 @@
-const myModal = document.getElementById('setupModal')
-const myInput = document.getElementById('myInput')
-
-myModal.addEventListener('shown.bs.modal', function () {
-	myInput.focus()
-})
