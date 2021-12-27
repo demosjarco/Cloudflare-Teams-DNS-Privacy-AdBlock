7e07d33b98dbb48e9a3345104f2d27ab61bfbf1f
Victor <1209494+demosjarco@users.noreply.github.com>
2021-12-27 11:50:45 -0700

    Create the form individually per account

--- public/js/settings.js
+++ public/js/settings.js
@@ -10,14 +10,21 @@ $(function () {
 			$(`<button class="nav-link active" id="account-id-tab" data-bs-toggle="pill" data-bs-target="#account-id" type="button" role="tab" aria-controls="account-id" aria-selected="true" title="Account ID#">Account Name</button>`).insertBefore("div#settingsModal button#addAccount");
 
 			$(`<div class="tab-pane fade show active" id="account-id" role="tabpanel" aria-labelledby="account-id-tab">
-				<div class="mb-3">
-					<label for="account-id-id" class="col-form-label">Account ID:</label>
-					<input type="text" class="form-control" id="account-id-id" spellcheck="false" autocomplete="off" pattern="[\w\d]+" minlength="32" maxlength="32" aria-required="true" required />
-				</div>
-				<div class="mb-3">
-					<label for="account-id-key" class="col-form-label">API Key:</label>
-					<input type="password" class="form-control" id="account-id-key" spellcheck="false" autocomplete="off" pattern="[\w\d-_]/i" minlength="40" maxlength="40" aria-required="true" required />
-				</div>
+				<form onsubmit="return false;">
+					<div class="row mb-3">
+						<label for="account-id-id" class="col-form-label">Account ID:</label>
+						<input type="text" class="form-control" id="account-id-id" spellcheck="false" autocomplete="off" pattern="[\w\d]+" minlength="32" maxlength="32" aria-required="true" required />
+					</div>
+					<div class="row mb-3">
+						<label for="account-id-key" class="col-form-label">API Key:</label>
+						<input type="password" class="form-control" id="account-id-key" spellcheck="false" autocomplete="off" pattern="[\w\d-_]/i" minlength="40" maxlength="40" aria-required="true" required />
+					</div>
+					<div class="d-grid gap-2 d-md-flex justify-content-md-end">
+						<input class="btn btn-outline-danger" type="reset">
+						<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+						<input class="btn btn-primary" type="submit" value="Save">
+					</div>
+				</form>
 			</div>`).appendTo('div#settingsModal div#settings-nav-tabContent');
 
 			$('div#settingsModal div#account-id').tab('show');
