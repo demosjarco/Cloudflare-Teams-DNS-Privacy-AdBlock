919e42f46d3614e5dd80241c624fc320358b8e12
Victor <1209494+demosjarco@users.noreply.github.com>
2021-12-27 20:09:46 -0700

    Moved to external file

---
+++ public/js/cryptoMethods.js
@@ -0,0 +1,36 @@
+function generateKeys(callback) {
+	window.crypto.subtle.generateKey({
+		name: "RSA-OAEP",
+		modulusLength: "4096",
+		publicExponent: new Uint8Array([1, 0, 1]),
+		hash: "SHA-512"
+	}, false, ["encrypt", "decrypt"]).then((key) => {
+		//returns a keypair object
+		callback(key.privateKey, key.publicKey);
+	}).catch((err1) => {
+		console.error(err1);
+		window.crypto.subtle.generateKey({
+			name: "RSA-OAEP",
+			modulusLength: "4096",
+			publicExponent: new Uint8Array([1, 0, 1]),
+			hash: "SHA-384"
+		}, false, ["encrypt", "decrypt"]).then((key) => {
+			//returns a keypair object
+			callback(key.privateKey, key.publicKey);
+		}).catch((err2) => {
+			console.error(err2);
+			window.crypto.subtle.generateKey({
+				name: "RSA-OAEP",
+				modulusLength: "4096",
+				publicExponent: new Uint8Array([1, 0, 1]),
+				hash: "SHA-256"
+			}, false, ["encrypt", "decrypt"]).then((key) => {
+				//returns a keypair object
+				callback(key.privateKey, key.publicKey);
+			}).catch((err3) => {
+				console.error(err3);
+				callback(null, null);
+			});
+		});
+	});
+}
