a245071a9f7213d0ac23df957fc58b8c447a5f52
Victor <1209494+demosjarco@users.noreply.github.com>
2022-01-14 14:13:11 -0700

    Converted from wget to github.js

--- setupScripts/github.js
+++ setupScripts/github.js
@@ -3,6 +3,16 @@
 class GitHub {
 	constructor() {
 		const axios = require('axios').default;
+
+		axios.get('https://raw.githubusercontent.com/paroga/cbor-js/master/cbor.js', {
+			responseType: 'stream',
+			maxContentLength: 20000 // 20kb
+		}).catch((error) => {
+			throw error;
+		}).then((response) => {
+			const { createWriteStream } = require('fs');
+			response.data.pipe(createWriteStream('./public/js/cbor.js'));
+		});;
 	}
 }
 
