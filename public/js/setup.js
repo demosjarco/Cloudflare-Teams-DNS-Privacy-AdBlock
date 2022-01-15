31fe558f589a65de01a308a72ff6455fc302ecc1
Victor <1209494+demosjarco@users.noreply.github.com>
2022-01-14 21:15:26 -0700

    Create setup modal in setup init instead of always there

--- public/js/setup.js
+++ public/js/setup.js
@@ -5,9 +5,71 @@ import { LocalStorage } from './localStorage.js';
 export class Setup {
 	constructor() {
 		// TODO: Check if setup completed
-		new bootstrap.Modal($('div#setupModal')).show();
+		if (true) {
+			// Create modal dom
+			this.createModalDom();
+			$(function () {
+				// Show modal
+				new bootstrap.Modal($('div#setupModal')).show();
+			});
 
-		this.compatibilityTab();
+			// Run compatibility tab stuff
+			this.compatibilityTab();
+		}
+	}
+
+	createModalDom() {
+		$(function () {
+			$('body').append(`<!-- Setup Modal -->
+			<div class="modal fade" id="setupModal" tabindex="-1" aria-labelledby="setupModalLabel" aria-hidden="true">
+				<div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+					<div class="modal-content">
+						<div class="modal-header bg-light">
+							<h5 class="modal-title" id="setupModalLabel">Setup</h5>
+							<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+						</div>
+						<div class="modal-body bg-body">
+							<nav>
+								<div class="nav nav-tabs justify-content-center" id="setup-nav-tab" role="tablist">
+									<button class="nav-link active" id="setup-nav-compatibility-tab" data-bs-toggle="tab" data-bs-target="#setup-nav-compatibility" type="button" role="tab" aria-controls="setup-nav-compatibility" aria-selected="true"><i class="fa-solid fa-list-check"></i></button>
+									<button class="nav-link disabled" id="setup-nav-security-tab" data-bs-toggle="tab" data-bs-target="#setup-nav-security" type="button" role="tab" aria-controls="setup-nav-security" aria-selected="false"><i class="fa-solid fa-user-shield"></i></button>
+									<button class="nav-link disabled" id="setup-nav-settings-tab" data-bs-toggle="tab" data-bs-target="#setup-nav-settings" type="button" role="tab" aria-controls="setup-nav-settings" aria-selected="false"><i class="fa-solid fa-gear"></i></button>
+									<button class="nav-link disabled" id="setup-nav-settings-tab" data-bs-toggle="tab" data-bs-target="#setup-nav-settings" type="button" role="tab" aria-controls="setup-nav-settings" aria-selected="false"><i class="fa-solid fa-gears"></i></button>
+								</div>
+							</nav>
+							<div class="tab-content" id="setup-nav-tabContent">
+								<div class="tab-pane show active" id="setup-nav-compatibility" role="tabpanel" aria-labelledby="setup-nav-compatibility-tab">
+									<div class="mt-3 mb-3">
+										<h6>Compatibility</h6>
+									</div>
+									<div class="mb-3" id="localStorage">
+										Local Storage
+									</div>
+									<div class="mb-3">
+										Web Crypto API
+									</div>
+									<div class="mb-3">
+										WebAuthn
+									</div>
+								</div>
+								<div class="tab-pane" id="setup-nav-security" role="tabpanel" aria-labelledby="setup-nav-security-tab">
+									<div class="m-3">
+										<h6>Security</h6>
+									</div>
+								</div>
+								<div class="tab-pane" id="setup-nav-settings" role="tabpanel" aria-labelledby="setup-nav-settings-tab">
+									<div class="m-3">
+										<h6>Settings</h6>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="modal-footer justify-content-start">
+						</div>
+					</div>
+				</div>
+			</div>`);
+		});
 	}
 
 	compatibilityTab() {
