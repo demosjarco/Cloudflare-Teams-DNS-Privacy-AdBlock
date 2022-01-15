08664810ee431585d5967d2c9d0a87fa29abf130
Victor <1209494+demosjarco@users.noreply.github.com>
2022-01-14 22:47:07 -0700

    More async

--- public/js/setup.js
+++ public/js/setup.js
@@ -86,12 +86,14 @@ export class Setup {
 		const localStorageAvailable = this.localStorageAvailable = new LocalStorage().availability;
 		$(() => {
 			$('div.modal#setupModal div#localStorage').append(`<span class="badge bg-${localStorageAvailable ? 'success' : 'danger'}"><i class="fa-solid fa-${localStorageAvailable ? 'check' : 'exclamation'}"></i></span>`);
-			if (!localStorageAvailable) {
+		});
+		if (!localStorageAvailable) {
+			$(() => {
 				$('div.modal#setupModal div#localStorage').append(`<div class="alert alert-danger mt-3" role="alert">
 					Please make sure your browser supports <a href="https://caniuse.com/mdn-api_window_localstorage">localStorage</a> and/or has permission (some browsers use the cookies control for localStorage too)
 				</div>`);
-			}
-		});
+			});
+		}
 	}
 
 	localEncryptionCompatibility(callback) {
