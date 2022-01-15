8fd683e9a6c763abdd3b8cabaeca82de55e52ace
Victor <1209494+demosjarco@users.noreply.github.com>
2022-01-14 21:09:32 -0700

    Add remediation tip

--- public/js/setup.js
+++ public/js/setup.js
@@ -14,6 +14,11 @@ export class Setup {
 		const localStorageAvailable = new LocalStorage().availability;
 		$(function () {
 			$('div.modal#setupModal div#localStorage').append(`<span class="badge bg-${localStorageAvailable ? 'success' : 'danger'}"><i class="fa-solid fa-${localStorageAvailable ? 'check' : 'exclamation'}"></i></span>`);
+			if (!localStorageAvailable) {
+				$('div.modal#setupModal div#localStorage').append(`<div class="alert alert-danger mt-3" role="alert">
+					Please make sure your browser supports <a href="https://caniuse.com/mdn-api_window_localstorage">localStorage</a> and/or has permission to do so (some browsers use the cookies control for localStorage too)
+				</div>`);
+			}
 		});
 	}
 }
