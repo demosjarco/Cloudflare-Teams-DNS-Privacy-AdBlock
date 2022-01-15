2f90977d50f03c41dffac512bf0258513a96a5c3
Victor <1209494+demosjarco@users.noreply.github.com>
2022-01-14 20:51:32 -0700

    Run stuff for first tab

--- public/js/setup.js
+++ public/js/setup.js
@@ -3,5 +3,10 @@ export class Setup {
 	constructor() {
 		// TODO: Check if setup completed
 		new bootstrap.Modal($('div#setupModal')).show();
+
+		this.compatibilityTab();
+	}
+
+	compatibilityTab() {
 	}
 }
