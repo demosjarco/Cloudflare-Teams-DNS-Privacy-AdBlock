3f15d60fe16ba85db5087e03c2a1387aba0839d0
Victor <1209494+demosjarco@users.noreply.github.com>
2022-01-14 21:54:26 -0700

    Changed wording

--- public/js/setup.js
+++ public/js/setup.js
@@ -80,7 +80,7 @@ export class Setup {
 			$('div.modal#setupModal div#localStorage').append(`<span class="badge bg-${localStorageAvailable ? 'success' : 'danger'}"><i class="fa-solid fa-${localStorageAvailable ? 'check' : 'exclamation'}"></i></span>`);
 			if (!localStorageAvailable) {
 				$('div.modal#setupModal div#localStorage').append(`<div class="alert alert-danger mt-3" role="alert">
-					Please make sure your browser supports <a href="https://caniuse.com/mdn-api_window_localstorage">localStorage</a> and/or has permission to do so (some browsers use the cookies control for localStorage too)
+					Please make sure your browser supports <a href="https://caniuse.com/mdn-api_window_localstorage">localStorage</a> and/or has permission (some browsers use the cookies control for localStorage too)
 				</div>`);
 			}
 		});
