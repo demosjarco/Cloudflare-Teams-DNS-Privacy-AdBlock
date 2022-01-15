aa0f32548395c6f658a5215f3539ceafb3f8561d
Victor <1209494+demosjarco@users.noreply.github.com>
2022-01-14 21:53:20 -0700

    Finished refactor

--- public/js/settings.js
+++ public/js/settings.js
@@ -1,11 +1,13 @@
 "use strict";
 
+import { LocalStorage } from './localStorage.js';
 // import { generateKeys, algorithmNameForId } from './cryptoMethods.js';
 
 export class Settings {
 	constructor() {
 		this.createButton();
 		this.createModalDom();
+		this.setupDiagnostics();
 	}
 
 	createButton() {
@@ -91,6 +93,74 @@ export class Settings {
 		});
 	}
 
+	setupDiagnostics() {
+		this.localStorageDiagnostics();
+		this.localEncryptionDiagnostics();
+		this.localAuthenticationDiagnostics();
+	}
+
+	localStorageDiagnostics() {
+		const localStorageAvailable = new LocalStorage().availability;
+		$(() => {
+			$("div#settingsModal span#localStorageAvailable").removeClass("bg-primary");
+			$("div#settingsModal span#localStorageAvailable").addClass(`bg-${localStorageAvailable ? 'success' : 'danger'}`);
+			if (!localStorageAvailable) {
+				$('div.modal#settingsModal div.modal-footer').append(`<div class="alert alert-danger mt-3" role="alert">
+					Please make sure your browser supports <a href="https://caniuse.com/mdn-api_window_localstorage">localStorage</a> and/or has permission (some browsers use the cookies control for localStorage too)
+				</div>`);
+			}
+		});
+		this.refreshTooltips();
+	}
+
+	localEncryptionDiagnostics() {
+		// encryptedStorageAvailable((available, algorithm) => {
+		// 	$(function () {
+		// 		if (available) {
+		// 			$("div#settingsModal span#localCryptoAvailable").removeClass("bg-primary");
+		// 			$("div#settingsModal span#localCryptoAvailable").addClass("bg-success");
+		// 			$("div#settingsModal span#localCryptoAvailable").prop("title", `Using ${algorithm.name} with ${algorithm.modulusLength} bits and ${algorithm.hash.name}`);
+		// 		} else {
+		// 			$("div#settingsModal span#localCryptoAvailable").removeClass("bg-primary");
+		// 			$("div#settingsModal span#localCryptoAvailable").addClass("bg-danger");
+		// 			$("div#settingsModal span#localCryptoAvailable").prop("title", `Not available`);
+		// 		}
+		// 		tooltipTriggerList.map(function (tooltipTriggerEl) {
+		// 			return new bootstrap.Tooltip(tooltipTriggerEl);
+		// 		});
+		// 	});
+		// });
+
+		// function encryptedStorageAvailable(testResult) {
+		// 	generateKeys((privateKey, publicKey) => {
+		// 		if (privateKey && publicKey) {
+		// 			testResult(true, privateKey.algorithm);
+		// 		} else {
+		// 			testResult(false);
+		// 		}
+		// 	});
+		// }
+	}
+
+	localAuthenticationDiagnostics() {
+		// generateKeys((algorithm) => {
+		// 	$(function () {
+		// 		if (algorithm) {
+		// 			$("div#settingsModal span#localKeysAvailable").removeClass("bg-primary");
+		// 			$("div#settingsModal span#localKeysAvailable").addClass("bg-success");
+		// 			$("div#settingsModal span#localKeysAvailable").prop("title", algorithmNameForId(algorithm));
+		// 		} else {
+		// 			$("div#settingsModal span#localKeysAvailable").removeClass("bg-primary");
+		// 			$("div#settingsModal span#localKeysAvailable").addClass("bg-danger");
+		// 			$("div#settingsModal span#localKeysAvailable").prop("title", `Not available. Please refresh and try your authentication again`);
+		// 		}
+		// 		tooltipTriggerList.map(function (tooltipTriggerEl) {
+		// 			return new bootstrap.Tooltip(tooltipTriggerEl);
+		// 		});
+		// 	});
+		// });
+	}
+
 	refreshTooltips() {
 		$(() => {
 			const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
@@ -100,100 +170,3 @@ export class Settings {
 		});
 	}
 }
-
-/*
-// Check for local storage availability
-if (storageAvailable('localStorage')) {
-	$(function () {
-		$("div#settingsModal span#localStorageAvailable").removeClass("bg-primary");
-		$("div#settingsModal span#localStorageAvailable").addClass("bg-success");
-		$("div#settingsModal span#localStorageAvailable").prop("title", `Local Storage available`);
-		tooltipTriggerList.map(function (tooltipTriggerEl) {
-			return new bootstrap.Tooltip(tooltipTriggerEl);
-		});
-	});
-}
-else {
-	$(function () {
-		$("div#settingsModal span#localStorageAvailable").removeClass("bg-primary");
-		$("div#settingsModal span#localStorageAvailable").addClass("bg-danger");
-		$("div#settingsModal span#localStorageAvailable").prop("title", `Local Storage not available. Check that cookies are enabled.`);
-		tooltipTriggerList.map(function (tooltipTriggerEl) {
-			return new bootstrap.Tooltip(tooltipTriggerEl);
-		});
-	});
-}
-
-// Check for local crypto availability
-encryptedStorageAvailable((available, algorithm) => {
-	$(function () {
-		if (available) {
-			$("div#settingsModal span#localCryptoAvailable").removeClass("bg-primary");
-			$("div#settingsModal span#localCryptoAvailable").addClass("bg-success");
-			$("div#settingsModal span#localCryptoAvailable").prop("title", `Using ${algorithm.name} with ${algorithm.modulusLength} bits and ${algorithm.hash.name}`);
-		} else {
-			$("div#settingsModal span#localCryptoAvailable").removeClass("bg-primary");
-			$("div#settingsModal span#localCryptoAvailable").addClass("bg-danger");
-			$("div#settingsModal span#localCryptoAvailable").prop("title", `Not available`);
-		}
-		tooltipTriggerList.map(function (tooltipTriggerEl) {
-			return new bootstrap.Tooltip(tooltipTriggerEl);
-		});
-	});
-});
-
-webauthnAvailable();
-
-function storageAvailable(type) {
-	var storage;
-	try {
-		storage = window[type];
-		var x = '__storage_test__';
-		storage.setItem(x, x);
-		storage.removeItem(x);
-		return true;
-	}
-	catch (e) {
-		return e instanceof DOMException && (
-			// everything except Firefox
-			e.code === 22 ||
-			// Firefox
-			e.code === 1014 ||
-			// test name field too, because code might not be present
-			// everything except Firefox
-			e.name === 'QuotaExceededError' ||
-			// Firefox
-			e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
-			// acknowledge QuotaExceededError only if there's something already stored
-			(storage && storage.length !== 0);
-	}
-}
-
-function encryptedStorageAvailable(testResult) {
-	// generateKeys((privateKey, publicKey) => {
-	// 	if (privateKey && publicKey) {
-	// 		testResult(true, privateKey.algorithm);
-	// 	} else {
-	// 		testResult(false);
-	// 	}
-	// });
-}
-
-function webauthnAvailable(testResult) {
-	generateKeys((algorithm) => {
-		$(function () {
-			if (algorithm) {
-				$("div#settingsModal span#localKeysAvailable").removeClass("bg-primary");
-				$("div#settingsModal span#localKeysAvailable").addClass("bg-success");
-				$("div#settingsModal span#localKeysAvailable").prop("title", algorithmNameForId(algorithm));
-			} else {
-				$("div#settingsModal span#localKeysAvailable").removeClass("bg-primary");
-				$("div#settingsModal span#localKeysAvailable").addClass("bg-danger");
-				$("div#settingsModal span#localKeysAvailable").prop("title", `Not available. Please refresh and try your authentication again`);
-			}
-			tooltipTriggerList.map(function (tooltipTriggerEl) {
-				return new bootstrap.Tooltip(tooltipTriggerEl);
-			});
-		});
-	});
-}*/
