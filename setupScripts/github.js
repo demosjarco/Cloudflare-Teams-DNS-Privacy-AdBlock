755a2859289c2c85dbc1d5baca924107df90cede
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2022-10-17 13:18:58 -0700

    Better error logging

--- setupScripts/github.js
+++ setupScripts/github.js
@@ -16,7 +16,22 @@ class GitHub {
 			responseType: 'stream',
 			maxContentLength: 20000, // 20kb
 		}).catch((error) => {
-			throw error;
+			if (error.response) {
+				// The request was made and the server responded with a status code
+				// that falls out of the range of 2xx
+				console.error(error.response.data);
+				console.error(error.response.status);
+				console.error(error.response.headers);
+			} else if (error.request) {
+				// The request was made but no response was received
+				// `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+				// http.ClientRequest in node.js
+				console.error(error.request);
+			} else {
+				// Something happened in setting up the request that triggered an Error
+				console.error('Error', error.message);
+			}
+			throw error.config;
 		}).then((response) => {
 			response.data.pipe(endFile);
 			response.data.pipe(hash);
