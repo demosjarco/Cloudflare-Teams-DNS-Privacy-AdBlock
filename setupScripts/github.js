67f6784c0a5b09af73dbc8f63b49371304dda3d6
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2023-04-17 13:09:37 -0700

    Rewrite using native `fetch` instead of `axios`

--- setupScripts/github.js
+++ setupScripts/github.js
@@ -2,51 +2,34 @@
 
 class GitHub {
 	constructor() {
-		const axios = require('axios');
 		const { createWriteStream } = require('node:fs');
 		const { createHash } = require('node:crypto');
+		const { pipeline } = require('node:stream/promises');
 
 		const endFile = createWriteStream('./public/js/cbor.js');
 		const hashFormat = 'sha512';
 		const hash = createHash(hashFormat);
 
-		axios({
-			url: '/paroga/cbor-js/master/cbor.js',
-			method: 'get',
-			baseURL: 'https://raw.githubusercontent.com',
-			responseType: 'stream',
-			maxContentLength: 20000, // 20kb
-		}).catch((error) => {
-			if (error.response) {
-				// The request was made and the server responded with a status code
-				// that falls out of the range of 2xx
-				console.error(error.response.data);
-				console.error(error.response.status);
-				console.error(error.response.headers);
-			} else if (error.request) {
-				// The request was made but no response was received
-				// `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-				// http.ClientRequest in node.js
-				console.error(error.request);
-			} else {
-				// Something happened in setting up the request that triggered an Error
-				console.error('Error', error.message);
-			}
-			throw error.config;
-		}).then((response) => {
-			response.data.pipe(endFile);
-			response.data.pipe(hash);
-		}).finally(() => {
-			endFile.once('close', () => {
-				if (process.env.NODE_ENV === 'development') {
-					console.log(`Downloaded ${'https://raw.githubusercontent.com/paroga/cbor-js/master/cbor.js'} to ${'./public/js/cbor.js'}`);
-				} else {
-					console.log('Downloaded cbor');
+		fetch('https://raw.githubusercontent.com/paroga/cbor-js/master/cbor.js')
+			.catch((error) => {
+				throw error;
+			})
+			.then((response) => {
+				if (!response.ok) {
+					throw new Error(`Error fetching ${url}: ${response.status} ${response.statusText}`);
 				}
-				
-				this.writeHTML('cbor', 'js', './js/cbor.js', `${hashFormat}-${hash.digest().toString('base64')}`);
+
+				// console.log(response.clone().body.constructor.name, response.clone().body.getReader().constructor.name);
+				Promise.all([pipeline(response.clone().body, endFile), pipeline(response.clone().body, hash)]).then(() => {
+					if (process.env.NODE_ENV === 'development') {
+						console.log(`Downloaded ${'https://raw.githubusercontent.com/paroga/cbor-js/master/cbor.js'} to ${'./public/js/cbor.js'}`);
+					} else {
+						console.log('Downloaded cbor');
+					}
+
+					this.writeHTML('cbor', 'js', './js/cbor.js', `${hashFormat}-${hash.digest().toString('base64')}`);
+				});
 			});
-		});
 	}
 
 	writeHTML(libraryName, libraryType, filePath, sri) {
